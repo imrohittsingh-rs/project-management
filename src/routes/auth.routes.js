fc6dd5827feb7548487b7315ca68759b87497221
Rohit Singh <imrohittsingh.rs@gmail.com>
2026-09-19 14:52:55 +0530

    completed phase 1

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,20 +3,38 @@ import {
   handleUserRegister,
   handleUserLogin,
   handleUserLogout,
+  getCurrentUser,
+  verifyEmail,
+  resendEmailVerification,
+  refreshAccessToken,
+  forgotPasswordRequest,
+  resetPassword,
+  changeCurrentPassword,
 } from "../controllers/auth.contollers.js";
 import { validate } from "../middlewares/validator.middleware.js";
 import {
   userRegisterValidator,
   userLoginValidator,
+  userChangeCurrentPasswordValidator,
+  userForgotPasswordValidator,
+  userResetPasswordValidator,
 } from "../validators/index.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
+// unsecured routes
 router.post("/register", userRegisterValidator(), validate, handleUserRegister);
 router.post("/login", userLoginValidator(), validate, handleUserLogin);
+router.get("/verify-email/:verificationToken", verifyEmail);
+router.post("/refresh-token", refreshAccessToken);
+router.post("/forgot-password", userForgotPasswordValidator(), validate, forgotPasswordRequest);
+router.post("/reset-password/:resetToken", userResetPasswordValidator(), validate, resetPassword);
 
 // secured routes
 router.post("/logout", verifyJWT, handleUserLogout);
+router.post("/current-user", verifyJWT, getCurrentUser);
+router.post("/change-password", verifyJWT, userChangeCurrentPasswordValidator(), validate, changeCurrentPassword);
+router.post("/resend-verification-email", verifyJWT, resendEmailVerification);
 
 export default router;
--- src/validators/index.js
+++ src/validators/index.js
@@ -33,21 +33,58 @@ const userRegisterValidator = () => {
 
 const userLoginValidator = () => {
   return [
-    body("username")
-      .trim()
-      .notEmpty()
-      .withMessage("Username is required"),
+    body("username").trim().notEmpty().withMessage("Username is required"),
     body("email")
       .trim()
       .notEmpty()
       .withMessage("Email is required")
       .isEmail()
       .withMessage("Invalid email address"),
-    body("password")
-      .trim()
-      .notEmpty()
-      .withMessage("Password is required"),
+    body("password").trim().notEmpty().withMessage("Password is required"),
   ];
 };
 
-export { userRegisterValidator, userLoginValidator };
+const userChangeCurrentPasswordValidator = () => {
+  return [
+    body("oldPassword")
+      .trim()
+      .notEmpty()
+      .withMessage("Old password is required"),
+    body("newPassword")
+      .trim()
+      .notEmpty()
+      .withMessage("New password is required")
+      .isLength({ min: 8, max: 100 })
+      .withMessage("New password must be between 8 and 100 characters"),
+  ];
+};
+
+const userForgotPasswordValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Invalid email address"),
+  ];
+};
+
+const userResetPasswordValidator = () => {
+  return [
+    body("newPassword")
+      .trim()
+      .notEmpty()
+      .withMessage("New password is required")
+      .isLength({ min: 8, max: 100 })
+      .withMessage("New password must be between 8 and 100 characters"),
+  ];
+};
+
+export {
+  userRegisterValidator,
+  userLoginValidator,
+  userChangeCurrentPasswordValidator,
+  userForgotPasswordValidator,
+  userResetPasswordValidator,
+};
