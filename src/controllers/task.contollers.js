6ebd800f87e65eb9ebb244722624fd25585b3c7b
Rohit Singh <imrohittsingh.rs@gmail.com>
2026-09-24 23:54:06 +0530

    task retrieval and creation logic

--- src/controllers/task.contollers.js
+++ src/controllers/task.contollers.js
@@ -9,19 +9,152 @@ import mongoose from "mongoose";
 import { AvailableUserRole, UserRoleEnum } from "../utils/constants.js";
 
 const getTasks = asyncHandler(async (req, res) => {
-  //test
+  const { projectId } = req.params;
+
+  const project = await Project.findById(projectId);
+  if (!project) {
+    throw new ApiError(404, "Project not found");
+  }
+
+  const tasks = await Task.find({
+    project: new mongoose.Types.ObjectId(projectId),
+  }).populate("assignedTo", "avatar username fullName");
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tasks, "Tasks fetched successfully"));
 });
 
 const createTask = asyncHandler(async (req, res) => {
-  
+  const { title, description, assignedTo, status } = req.body;
+  const { projectId } = req.params;
+
+  const project = await Project.findById(projectId);
+  if (!project) {
+    throw new ApiError(404, "Project not found");
+  }
+
+  const files = req.files || [];
+  const attachments = files.map((file) => {
+    return {
+      url: `${process.env.SERVER_URL}/images/${file.originalname}`,
+      mimetype: file.mimetype,
+      size: file.size,
+    };
+  });
+
+  const task = await Task.create({
+    title,
+    description,
+    assignedTo: assignedTo ? new mongoose.Types.ObjectId(assignedTo) : null,
+    assignedBy: new mongoose.Types.ObjectId(req.user._id),
+    status,
+    project: new mongoose.Types.ObjectId(projectId),
+    attachments,
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, task, "Task created successfully"));
 });
 
 const getTaskById = asyncHandler(async (req, res) => {
-  //test
+  const { taskId } = req.params;
+  const task = await Task.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(taskId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "assignedTo",
+        foreignField: "_id",
+        as: "assignedTo",
+      },
+      pipeline: [
+        {
+          $project: {
+            _id: 1,
+            username: 1,
+            fullName: 1,
+            avatar: 1,
+          },
+        },
+      ],
+    },
+    {
+      $lookup: {
+        from: "subtasks",
+        localField: "_id",
+        foreignField: "task",
+        as: "subtasks",
+      },
+      pipeline: [
+        {
+          $lookup: {
+            from: "users",
+            localField: "assignedTo",
+            foreignField: "_id",
+            as: "createdBy",
+            pipeline: [
+              {
+                $project: {
+                  _id: 1,
+                  username: 1,
+                  fullName: 1,
+                  avatar: 1,
+                },
+              },
+            ],
+          },
+        },
+        {
+          $addFields: {
+            createdBy: { $arrayElemAt: ["$createdBy", 0] },
+          },
+        },
+      ],
+    },
+    {
+      $addFields: {
+        assignedTo: { $arrayElemAt: ["$assignedTo", 0] },
+      },
+    },
+  ]);
+
+  if (!task || task.length === 0) {
+    throw new ApiError(404, "Task not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, task[0], "Task fetched successfully"));
 });
 
 const updateTask = asyncHandler(async (req, res) => {
-  //test
+  const { taskId } = req.params;
+  const { title, description, assignedTo, status } = req.body;
+
+  const task = await Task.findById(taskId);
+  if (!task) {
+    throw new ApiError(404, "Task not found");
+  }
+
+  const updatedTask = await Task.findByIdAndUpdate(
+    taskId,
+    {
+      title,
+      description,
+      assignedTo: assignedTo ? new mongoose.Types.ObjectId(assignedTo) : null,
+      status,
+    },
+    { new: true },
+  );
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedTask, "Task updated successfully"));
 });
 
 const deleteTask = asyncHandler(async (req, res) => {
